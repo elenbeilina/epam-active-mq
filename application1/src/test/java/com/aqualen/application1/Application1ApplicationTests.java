6fb1963fc06f521eed8c1f719483977186f0703f
beiliele <Elena.Beilina@externals.adidas.com>
2022-08-25 19:26:25 +0300

    added applications

---
+++ application1/src/test/java/com/aqualen/application1/Application1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aqualen.application1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
